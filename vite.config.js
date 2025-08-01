2601d69f0391099860fa04860d744e9777cd7560
Bastien Sarolea <bastien.sarolea@gmail.com>
2025-08-01 15:54:22 +0200

    02

--- vite.config.js
+++ vite.config.js
@@ -22,8 +22,7 @@ export default defineConfig({
       'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
-  base: '/', // This makes all asset paths relative instead of absolute
-  // base: './', // This makes all asset paths relative instead of absolute
+  base: './', // This makes all asset paths relative instead of absolute
   worker: {
     format: 'es',
     plugins: [],
